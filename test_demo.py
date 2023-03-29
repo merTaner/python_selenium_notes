200f8cfdb410c6a91bb57b7a76d3b7f66c9d464a
merTaner <merttaner44@gmail.com>
2023-03-29 16:47:13 +0300

    create daily date folder and clean code

--- test_demo.py
+++ test_demo.py
@@ -5,23 +5,27 @@ from time import sleep
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import pytest
+from pathlib import Path
+from datetime import date
 
 class Test_Demo:
     def setup_method(self):
         self.driver = webdriver.Chrome(ChromeDriverManager().install())
         self.driver.maximize_window()
         self.driver.get("https://www.saucedemo.com/")
+        self.forlder_path_of_name = str(date.today()) 
+        Path(self.forlder_path_of_name).mkdir(exist_ok=True)
 
     def teardown_method(self):
         self.driver.quit()
     
     @pytest.mark.parametrize("username, pasword", [("1", "1"), ("root", "root")])
     def test_invalid_login(self, username, pasword):
-        WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located((By.ID, "user-name")))
+        self.visibility_element_located((By.ID, "user-name"))
         user_name = self.driver.find_element(By.ID, "user-name")
         user_name.send_keys(username)
 
-        WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located((By.ID, "password")))
+        self.visibility_element_located((By.ID, "password"))
         password = self.driver.find_element(By.ID, "password")
         password.send_keys(pasword)
 
@@ -30,6 +34,8 @@ class Test_Demo:
 
         error_message = self.driver.find_element(By.XPATH, "//*[@id='login_button_container']/div/form/div[3]/h3")
         
+        self.driver.save_screenshot(self.forlder_path_of_name+f"/invalid-test-for-{username}-{pasword}.png")
         assert error_message.text == "Epic sadface: Username and password do not match any user in this service"
 
-   
+    def visibility_element_located(self, located):
+        WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located((located)))
